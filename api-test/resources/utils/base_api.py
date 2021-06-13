5f022ee3732719f94667e22fb58109c4d840a6fb
joaolucasfernandes <joaolucasfernandes@gmail.com>
2021-06-13 19:06:17 -0300

    Improving comments about the hardcoded tokne

--- api-test/resources/utils/base_api.py
+++ api-test/resources/utils/base_api.py
@@ -9,8 +9,8 @@ class BaseApi:
         """
         I changed the token passed on postman collection and use my own personal token from Go Rest Api... 
         I do this for avoid authentication failures due to token reset by the api creator.
-        This can be replaced by a request to an auth endpoint that returns a token given a user/password.
-        For security reasons, this user and password should be obtained from environment variables also.
+        The token value that are hardcoded here can be replaced by a request to an auth endpoint that returns a token given a user/password(I did not find it for this api).
+        For security reasons, this user and password should be obtained from environment variables or secrets(when talking about docker/kubernetes environment) also.
         """
         return 'Bearer 4036e8a535449c1e91050813efe1a72e0d10150c410e152295b82b36e362f310'
 
