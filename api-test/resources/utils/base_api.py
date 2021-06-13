89625ec8efc84204ca682e0a4aead7c4adf52a8f
joaolucasfernandes <joaolucasfernandes@gmail.com>
2021-06-13 17:13:25 -0300

    Changing auth token to my own auth token

--- api-test/resources/utils/base_api.py
+++ api-test/resources/utils/base_api.py
@@ -7,10 +7,12 @@ class BaseApi:
 
     def get_auth_token(self):
         """
+        I changed the token passed on postman collection and use my own personal token from Go Rest Api... 
+        I do this for avoid authentication failures due to token reset by the api creator.
         This can be replaced by a request to an auth endpoint that returns a token given a user/password.
         For security reasons, this user and password should be obtained from environment variables also.
         """
-        return 'Bearer 2275e2cbbf8dc1d113b25fb018cdb2e07e088b35bb5f7b7c13ca160ed96a82ba'
+        return 'Bearer 4036e8a535449c1e91050813efe1a72e0d10150c410e152295b82b36e362f310'
 
     def get_default_headers(self, token):
 
